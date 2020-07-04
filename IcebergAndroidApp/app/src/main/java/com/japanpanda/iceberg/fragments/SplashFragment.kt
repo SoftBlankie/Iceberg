5c2e465a1f27e05407845019cf80d0ad23433b70
Vincent <japanxpanda@yahoo.com>
2020-07-03 20:12:38 -0700

    Moved and reordered location

--- IcebergAndroidApp/app/src/main/java/com/japanpanda/iceberg/fragments/SplashFragment.kt
+++ IcebergAndroidApp/app/src/main/java/com/japanpanda/iceberg/fragments/SplashFragment.kt
@@ -1,12 +1,12 @@
-package com.japanpanda.iceberg
+package com.japanpanda.iceberg.fragments
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.japanpanda.iceberg.R
 
 class SplashFragment : Fragment() {
 
