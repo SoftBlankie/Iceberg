88f4d77bc1120bf28f30fc07eb6e5c8c6d22f9de
Vincent <japanxpanda@yahoo.com>
2020-07-07 04:01:51 -0700

    Incorporated react-redux

--- icebergwebapp/src/index.js
+++ icebergwebapp/src/index.js
@@ -1,15 +1,22 @@
+import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Root from './components/Root';
 import './index.css';
+import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
-import { ThemeProvider } from '@material-ui/core';
 import theme from './theme';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Root store={store} />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
