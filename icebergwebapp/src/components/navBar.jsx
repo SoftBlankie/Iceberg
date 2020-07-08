4c4a2469901fc6108f2bc731e65ba8b3370674ca
Vincent <japanxpanda@yahoo.com>
2020-07-08 02:31:03 -0700

    Added conditional signout/dashboard button as well as signout function

--- icebergwebapp/src/components/navBar.jsx
+++ icebergwebapp/src/components/navBar.jsx
@@ -9,6 +9,9 @@ import {
 import { withStyles } from '@material-ui/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../actions';
+import { logoutUser } from '../services/authService';
 
 const styles = (theme) => ({
   root: {
@@ -28,14 +31,16 @@ const styles = (theme) => ({
 });
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { login: true };
-  }
+  signOut = () => {
+    // update state
+    this.props.logout();
+    // clear httponly cookie
+    logoutUser();
+  };
 
   render() {
     const { classes } = this.props;
-
+    let isLogged = this.props.loggedIn;
     return (
       <div className={classes.root}>
         <AppBar position="static">
@@ -63,12 +68,26 @@ class NavBar extends Component {
             </Button>
 
             <section className={classes.rightToolbar}>
-              <Button component={Link} to={'/login'} color="inherit">
-                Login
-              </Button>
-              <Button component={Link} to={'/signup'} color="inherit">
-                Sign Up
-              </Button>
+              {!isLogged && (
+                <div>
+                  <Button component={Link} to={'/login'} color="inherit">
+                    Login
+                  </Button>
+                  <Button component={Link} to={'/signup'} color="inherit">
+                    Sign Up
+                  </Button>
+                </div>
+              )}
+              {isLogged && (
+                <div>
+                  <Button component={Link} to={'/dashboard'} color="inherit">
+                    Dashboard
+                  </Button>
+                  <Button onClick={this.signOut} color="inherit">
+                    Sign Out
+                  </Button>
+                </div>
+              )}
             </section>
           </Toolbar>
         </AppBar>
@@ -77,4 +96,17 @@ class NavBar extends Component {
   }
 }
 
-export default withStyles(styles)(NavBar);
+const mapStateToProps = (state) => {
+  return { loggedIn: state.loggedIn };
+};
+
+const mapDispatchToProps = () => {
+  return {
+    logout,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps()
+)(withStyles(styles)(NavBar));
